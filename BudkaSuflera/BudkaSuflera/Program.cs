3fb78e25c79c4cdb605c68a55b7e1c968c684557
jerzyjerzy8 <jerzymatlakiewicz@gmail.com>
2019-02-17 21:55:07 +0800

    Budka Suflera. HashSet<string> nie przyjmuje duplikatow

---
+++ BudkaSuflera/BudkaSuflera/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BudkaSuflera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Out.WriteLine(IsSpace(' '));
+
+            CorrectCugowski("1 2 3\n-1 -1 -2 -3 -4");
+
+            Console.In.ReadLine();
+        }
+
+        static void CorrectCugowski(string input)
+        {
+            string[] inputArr = input.Split('\n');
+
+            HashSet<string> originalSet = new HashSet<string>();
+            HashSet<string> attemptSet = new HashSet<string>();
+
+            foreach (string el in inputArr[0].Split(' '))
+            {
+                originalSet.Add(el);
+            }
+            foreach (string el in inputArr[1].Split(' '))
+            {
+                attemptSet.Add(el);
+            }
+
+            foreach (string el in originalSet)
+            {
+                Console.Out.WriteLine(el);
+            }
+            Console.Out.WriteLine();
+            foreach (string el in attemptSet)
+            {
+                Console.Out.WriteLine(el);
+            }
+        }
+
+        static bool IsSpace(char ch)
+        {
+            return new Regex(@"^ $").IsMatch(ch.ToString());
+        }
+    }
+}
