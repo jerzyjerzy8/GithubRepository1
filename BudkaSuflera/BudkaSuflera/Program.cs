e73f49563196dfdde73a799bf5d713301a33da71
jerzyjerzy8 <jerzymatlakiewicz@gmail.com>
2019-02-19 21:10:12 +0800

    CorrectCugowski kompletna metoda

--- BudkaSuflera/BudkaSuflera/Program.cs
+++ BudkaSuflera/BudkaSuflera/Program.cs
@@ -13,37 +13,64 @@ namespace BudkaSuflera
         {
             //Console.Out.WriteLine(IsSpace(' '));
 
-            CorrectCugowski("1 2 3\n-1 -1 -2 -3 -4");
+            CorrectCugowski("JOLKA JOLKA PAMIETASZ LATO ZE SNOW\nJOLKA ZE SNOW");
 
             Console.In.ReadLine();
         }
+
 
         static void CorrectCugowski(string input)
         {
             string[] inputArr = input.Split('\n');
 
-            HashSet<string> originalSet = new HashSet<string>();
-            HashSet<string> attemptSet = new HashSet<string>();
+            Dictionary<string, int> originalDict = GetDictionaryFromString(inputArr[0]);
+            Dictionary<string, int> attemptDict = GetDictionaryFromString(inputArr[1]);
 
-            foreach (string el in inputArr[0].Split(' '))
+            List<string> differenceList = new List<string>();
+
+            foreach (KeyValuePair<string, int> orPair in originalDict)
             {
-                originalSet.Add(el);
-            }
-            foreach (string el in inputArr[1].Split(' '))
-            {
-                attemptSet.Add(el);
+                if (!attemptDict.ContainsKey(orPair.Key) || attemptDict[orPair.Key] != orPair.Value)
+                {
+                    int numberOfWords = attemptDict.ContainsKey(orPair.Key)? orPair.Value - attemptDict[orPair.Key] : orPair.Value;
+                    for (int i = 0; i < numberOfWords; i++)
+                    {
+                        differenceList.Add(orPair.Key);
+                    }
+                }
             }
 
-            foreach (string el in originalSet)
+            differenceList.Sort();
+
+            Console.Out.WriteLine(differenceList.Count);
+
+            foreach (string el in differenceList)
             {
                 Console.Out.WriteLine(el);
             }
-            Console.Out.WriteLine();
-            foreach (string el in attemptSet)
-            {
-                Console.Out.WriteLine(el);
-            }
+
         }
+
+
+        static Dictionary<string, int> GetDictionaryFromString(string s)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (string word in s.Split(' '))
+            {
+                if (!dict.ContainsKey(word))
+                {
+                    dict.Add(word, 1);
+                }
+                else
+                {
+                    dict[word] += 1;
+                }
+            }
+
+            return dict;
+        }
+
 
         static bool IsSpace(char ch)
         {
