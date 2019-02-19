1c0e2facb9b08338c871f054aaed3c253b12fcc6
jerzyjerzy8 <jerzymatlakiewicz@gmail.com>
2019-02-19 21:59:59 +0800

    dodano wersje na SPOJ.COM (zakomentowane)

--- BudkaSuflera/BudkaSuflera/Program.cs
+++ BudkaSuflera/BudkaSuflera/Program.cs
@@ -78,3 +78,61 @@ namespace BudkaSuflera
         }
     }
 }
+
+/*
+using System;
+using System.Collections.Generic;
+
+public class Test
+{
+    public static void Main()
+    {
+        Dictionary<string, int> originalDict = GetDictionaryFromString(Console.In.ReadLine());
+        Dictionary<string, int> attemptDict = GetDictionaryFromString(Console.In.ReadLine());
+
+        List<string> differenceList = new List<string>();
+
+        foreach (KeyValuePair<string, int> orPair in originalDict)
+        {
+            if (!attemptDict.ContainsKey(orPair.Key) || attemptDict[orPair.Key] != orPair.Value)
+            {
+                int numberOfWords = attemptDict.ContainsKey(orPair.Key) ? orPair.Value - attemptDict[orPair.Key] : orPair.Value;
+                for (int i = 0; i < numberOfWords; i++)
+                {
+                    differenceList.Add(orPair.Key);
+                }
+            }
+        }
+
+        differenceList.Sort();
+
+        Console.Out.WriteLine(differenceList.Count);
+
+        foreach (string el in differenceList)
+        {
+            Console.Out.WriteLine(el);
+        }
+    }
+
+
+    static Dictionary<string, int> GetDictionaryFromString(string s)
+    {
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+        foreach (string word in s.Split(' '))
+        {
+            if (!dict.ContainsKey(word))
+            {
+                dict.Add(word, 1);
+            }
+            else
+            {
+                dict[word] += 1;
+            }
+        }
+
+        return dict;
+    }
+}
+
+*/
